a1e8cba0364a3843e7ed487160ca429a3a923714
allinthemind66 <r.nilon92@gmail.com>
2018-09-14 16:58:02 -0400

    adds initial game start

--- level1.rb
+++ level1.rb
@@ -28,10 +28,25 @@ def world_tag
   culpa qui officia deserunt mollit anim id est laborum."
 end
 
+def game_start
+  puts "In the darkness you awaken. You find yourself in a strange place that is
+  both familiar and unfamiliar in equal measure. Your arms feel haggard and sore
+  and as you look around you notice shackles on the wall that have been somehow destroyed.
+  With your arms free you stand up and really look at the room in more detail."
 
+  puts "The room is a large dimly lit chamber. Along the walls you notice sconces
+  containing torches. The light from these torches is barely strong enough to illuminate
+  the closest parts of the dungeon."
+
+  puts "Along the opposite wall you see a wooden door, that has a barred porthole.
+  To your right you see what appears to be a man who is shackled to the wall.
+  To the left there is just an empty wall."
+
+end
 def app
   title
   world_tag
+  game_start
 end
 
 app
