c54a99252e1ee203c9b902568aa6990579d2a278
Matt <sotek222@yahoo.com>
2018-09-14 23:10:02 -0400

    fixes text formatting & changes flavor text for intro

--- level1.rb
+++ level1.rb
@@ -28,26 +28,31 @@ def title
 # This is the explanation of the game. Mood setting tag that
 # will be before every game set in the halberd universe
 def world_tag
-  puts "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in '
-  culpa qui officia deserunt mollit anim id est laborum."
+  puts "                                                                        "
+  puts "                                                                        "
+  puts "In a grim and perlious world the darkness consumes all. Brave adventurers stalk forgotten halls
+  deep underground in the hopes of finding their fortunes. Armies march in war ravaged lands for dominion
+  over the scraps of land that remain untouched. Worst of all is the horror that lurks in the shadows, eternally
+  waiting for the time when they may consume all civilization. You may enter this world if you dare, but
+  be on your guard for the darkness awaits all!"
 end
 
 def game_start
+  puts "                                                                        "
+  puts "                                                                        "
   puts "In the darkness you awaken. You find yourself in a strange place that is
   both familiar and unfamiliar in equal measure. Your arms feel haggard and sore
   and as you look around you notice shackles on the wall that have been somehow destroyed.
-  With your arms free you stand up and really look at the room in more detail."
-
+  With your arms free you stand up and really look at your surroundings."
+  puts "                                                                        "
   puts "The room is a large dimly lit chamber. Along the walls you notice sconces
   containing torches. The light from these torches is barely strong enough to illuminate
-  the closest parts of the dungeon."
-
+  the closest parts of this room."
+  puts "                                                                        "
   puts "Along the opposite wall to the North you see a wooden door, that has a barred porthole.
   To your right on the eastern wall you see what appears to be a man who is shackled to the wall.
-  To the left on the western part of the room there is just an empty wall."
+  To the left on the western part of the room there is just a wall with a torch."
+  puts "                                                                        "
   puts "What would you like to do now?"
   initial_choice = gets.chomp
   level_1_choices = Level1StartRoomChoices.new.first_choice(initial_choice)
