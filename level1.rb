a3694226cbde00323bdc140bbb30264fd5244f03
allinthemind66 <r.nilon92@gmail.com>
2018-09-17 16:21:53 -0400

    comment out scroll text for game and level descriptions

--- level1.rb
+++ level1.rb
@@ -51,15 +51,16 @@ def title
 def world_tag
   puts "                                                                        "
   puts "                                                                        "
-  sentence = "In a grim and perlious world the darkness consumes all. Brave adventurers stalk forgotten halls
+  # sentence =
+  puts "In a grim and perlious world the darkness consumes all. Brave adventurers stalk forgotten halls
   deep underground in the hopes of finding their fortunes. Armies march in war ravaged lands for dominion
   over the scraps of land that remain untouched. Worst of all is the horror that lurks in the shadows, eternally
   waiting for the time when they may consume all civilization. You may enter this world if you dare, but
   be on your guard for the darkness awaits all!"
-  sentence.each_char do |c|
-    print c
-    sleep(0.05)
-  end
+  # sentence.each_char do |c|
+  #   print c
+  #   sleep(0.05)
+  # end
   # puts "In a grim and perlious world the darkness consumes all. Brave adventurers stalk forgotten halls
   # deep underground in the hopes of finding their fortunes. Armies march in war ravaged lands for dominion
   # over the scraps of land that remain untouched. Worst of all is the horror that lurks in the shadows, eternally
@@ -70,29 +71,32 @@ end
 def game_start
   puts "                                                                        "
   puts "                                                                        "
-  "In the darkness you awaken. You find yourself in a strange place that is
+  puts "In the darkness you awaken. You find yourself in a strange place that is
   both familiar and unfamiliar in equal measure. Your arms feel haggard and sore
   and as you look around you notice shackles on the wall that have been somehow destroyed.
-  With your arms free you stand up and really look at your surroundings.".each_char do |c|
-    print c
-    sleep (0.05)
-  end
+  # With your arms free you stand up and really look at your surroundings."
+  # .each_char do |c|
+  #   print c
+  #   sleep (0.05)
+  # end
   puts "\n"
   puts "                                                                        "
-  "The room is a large dimly lit chamber. Along the walls you notice sconces
+  puts "The room is a large dimly lit chamber. Along the walls you notice sconces
   containing torches. The light from these torches is barely strong enough to illuminate
-  the closest parts of this room.".each_char do |c|
-    print c
-    sleep (0.05)
-  end
+  the closest parts of this room."
+  # .each_char do |c|
+  #   print c
+  #   sleep (0.05)
+  # end
   puts "\n"
   puts "                                                                        "
-  "Along the opposite wall to the North you see a wooden door, that has a barred porthole.
+  puts "Along the opposite wall to the North you see a wooden door, that has a barred porthole.
   To your right on the eastern wall you see what appears to be a man who is shackled to the wall.
-  To the left on the western part of the room there is just an empty wall.".each_char do |c|
-    print c
-    sleep (0.05)
-  end
+  To the left on the western part of the room there is just an empty wall."
+  # .each_char do |c|
+  #   print c
+  #   sleep (0.05)
+  # end
   puts "                                                                        "
   puts "                                                                        "
   puts "                                                                        "
