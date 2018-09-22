85d891fcc81a6d6c41d1b5ae35868ecfcdd4e237
Matt <sotek222@yahoo.com>
2018-09-22 11:36:22 -0400

    adds more options to case statement in starting room

--- level1_start_room_choices.rb
+++ level1_start_room_choices.rb
@@ -9,10 +9,10 @@ class Level1StartRoomChoices < InputChoice
       puts "What would you like to do now?"
       choice = gets.chomp
       case choice
-      when "North"
+      when "forward", "north", "Forward", "North"
         scroll_text("You see a closed door.")
         puts "\n"
-      when "West"
+      when 'West', 'west', 'left', 'Left'
         if Player.all.first.inventory.include?("Torch")
           puts "You see an empty wall where a torch was."
         else
@@ -20,17 +20,17 @@ class Level1StartRoomChoices < InputChoice
           puts "\n"
         end
         puts "\n"
-      when "East"
+      when "East", 'east', 'Right', 'right'
         scroll_text("You look to your right and see a man shackled to the wall. Maybe you should go talk to him...")
         puts "\n"
-      when "help"
+      when "help", 'Help', 'HELP'
         puts '##############################################################'
         puts 'Directional commands: Up, down, left, right, forward, behind'
         puts '##############################################################'
         puts 'Other Commands: Talk, Open Door, open chest'
         puts '##############################################################'
         puts 'Game Commands: Map, Save, Quit Game'
-      when 'talk to man'
+      when 'talk to man', 'talk', 'Talk', 'man', 'Talk to Man', 'Talk to Man'
         @door_locked = false
         par =  "You walk over to the man and ask him his name. He responds, hesitantly and with some surprise,
         'What?! Its me Ragnir.'
@@ -40,9 +40,9 @@ class Level1StartRoomChoices < InputChoice
         ARRGHHHHHHHH!"
         scroll_text(par)
         puts "\n"
-      when "hello"
+      when "hello", 'Hello'
         puts 'A voice somewhere responds "Hi there."'
-      when "open door"
+      when "open door", "Open Door", "door", "Door", "open", "Open"
         if @door_locked === false
           scroll_text("You open the door")
           puts "\n"
@@ -50,7 +50,7 @@ class Level1StartRoomChoices < InputChoice
           scroll_text("You can't open the door. It's locked from the outside!")
           puts "\n"
         end
-      when "leave room"
+      when "leave room", 'Leave Room', 'Leave room', 'leave', 'Leave'
         if @door_locked === false
           scroll_text("You walk outside into a dimly lit hallway.")
           puts "\n"
@@ -60,7 +60,11 @@ class Level1StartRoomChoices < InputChoice
           scroll_text("You can't open the door. It's locked from the outside!")
           puts "\n"
         end
-      when "Take torch"
+      when 'Torch', 'torch'
+        puts 'The torch sits inside a sconce. It casts a pool of light around it.'
+        scroll_text ('Maybe it would be useful?')
+        puts "\n"
+      when "Take torch", 'Grab torch', 'grab torch', 'take torch'
         if Player.all.first.inventory.include?("Torch")
           puts "You have already taken the torch"
         else
